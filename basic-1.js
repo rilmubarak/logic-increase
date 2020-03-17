ccd4914b5e509bc5839fd56f6703bc611984de13
mubaraqhere <mubaraqhere@gmail.com>
2020-03-17 08:25:56 +0700

    Basic-1 : latihan if-else-1

---
+++ basic-1.js
@@ -0,0 +1,30 @@
+// Hints
+// Variabel tetap di-input secara manual.
+// Variabel namadan perandapat diisi apa saja.
+// Nama tidak perlu dicek sama persis seperti contoh input/output
+// Buat If-Elseberbeda masing-masing untuk mengecek peran
+
+let nama = ''
+let peran = 'Penyihir'
+let peranLengkap = ['Penyihir', 'Ksatria', 'Tabib']
+
+if (nama === '' && peran === ''){
+    console.log("Nama dan Peran harus diisi");
+} else if (nama && peran === ''){
+    console.log(`Hai ${nama}, Peran harus diisi`);
+} else if (nama && peran === 'Ksatria'){
+    console.log(`Selamat datang didunia proxytia, ${nama}`);
+    console.log(`Halo ${peran} ${nama}, kamu dapat menyerang dengan senjata mu!`);
+} else if (nama && peran === 'Tabib'){
+    console.log(`Selamat datang didunia Proxitia, ${nama}`);
+    console.log(`Halo ${peran} ${nama}, kamu akan membantu teman mu yang terluka`);
+} else if (nama && peran === 'Penyihir'){
+    console.log(`Selamat datang didunia Proxytia, ${nama}`);
+    console.log(`Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenangan mu!`);
+} else if (nama && peran !== peranLengkap){
+    console.log(`Hai ${nama} pilihlah peran yang sesuai, seperti: Penyihir, Tabib, dan Ksatria`);
+} else if (nama === '' && peran !== peranLengkap){
+    console.log(`Hai ${peran}, siapa nama mu? tolong diisi terlebih dahulu :)`);
+} else {
+    "Pilihannya kurang sesuai :("
+}
