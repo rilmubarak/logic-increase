c318b4f8b419ebe7442db327937d98f68cad0c01
mubaraqhere <mubaraqhere@gmail.com>
2020-05-13 21:09:16 +0700

    another exercise

---
+++ e.js
@@ -0,0 +1,43 @@
+/**
+ * 1. Display all active singers
+ * 2. Display all inactive singers
+ * 3. Display all genres that only have one singer
+ * 4. Display all genres that have two or more singers
+ * 5. Sort singers
+ */
+let WhitneyHouston = {
+    Name: 'Whitney Houston',
+    Genre: ['R&B', 'pop', 'soul', 'gospel', 'dance'],
+    Active: '1977-2012',
+  }
+  let MariahCarey = {
+    Name: 'Mariah Carey',
+    Genre: ['R&B', 'pop', 'hip hop', 'soul'],
+    Active: '1989-present',
+  }
+  let CocoLee = {
+    Name: 'Coco Lee',
+    Genre: ['pop', 'c-pop', 'dance-pop', 'hip hop', 'R&B', 'soul'],
+    Active: '1993-present',
+  }
+  let ChristinaPerri = {
+    Name: 'Christina Perri',
+    Genre: ['pop', 'pop rock'],
+    Active: '2008-present',
+  }
+  let MichaelJosephJackson = {
+    Name: 'Michael Joseph Jackson',
+    Genre: ['pop', 'soul', 'funk', 'rhythm and blues', 'rock', 'disco', 'post-disco', 'dance-pop', 'new jack swing'],
+    Active: '1964-2009',
+  }
+  let Singers = [WhitneyHouston, MariahCarey, CocoLee, ChristinaPerri, MichaelJosephJackson];
+  function activeSingers(input) {
+  }
+  function inactiveSingers(input) {
+  }
+  function oneGenre(input) {
+  }
+  function multipleGenres(input) {
+  }
+  function sortSingers() {
+  }
